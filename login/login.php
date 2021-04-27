444638f989441cba55293a0df0a29f7745a10598
Lotosney <sebastiandwojcik@gmail.com>
2021-04-27 21:11:34 +0200

    Add login form

---
+++ login/login.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Logowanie</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+
+            <li><a href="../registration/register.php">Rejestracja</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <h1>Zaloguj się</h1>
+    <p> Nie masz konta? <a href="../registration/register.php">Zarejestruj się!</a></p>
+
+    <form action="login/login-back.php" method="post">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Hasło">
+        <button type="submit" name="submit">Zaloguj się</button>
+    </form>
+
+</main>
+<?php
+require_once '../includes/footer.php';
+
+?>
